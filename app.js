495a136a92ce955be37df4e527ba6c0a8f9c2f03
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-04-09 09:36:58 -0600

    Add POST and GET requests for proxy server

---
+++ app.js
@@ -0,0 +1,73 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+const fetch = require('node-fetch');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+app.use(cors());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Post ocd_id to proxy server
+app.post('/api/search', (req, res) => {
+  app.locals.ocd_id = req.body.ocd_id;
+  // res.sendStatus(202);
+  res.end();
+});
+
+//Use proxy server to make external GET request to DemWorks API and send returned data back
+app.get('/api/elections', async (req, res) => {
+  try {
+    const url = app.locals.ocd_id;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+  res.end();
+});
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//listen on port 5000
+app.listen(app.get('port'), () => {
+  console.log(`Server is running on ${app.get('port')}`);
+});
+
+module.exports = app;
